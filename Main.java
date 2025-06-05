ab6aa22c1912b9e9dee09209021fd047d9e59782
tartegauri <tartegauri30@gmail.com>
2025-06-05 12:15:49 +0530

    this is third commit

--- Main.java
+++ Main.java
@@ -5,11 +5,3 @@ public class Main {
 }
 
 
-git init
-git add .
-git commit -m "Initial commit for Jenkins demo"
-git remote add origin <your-repo-url>
-git push -u origin main
-
-git config --global user.email "your.email@example.com"
-git config --global user.name "Your Name"
